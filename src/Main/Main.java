9d0b56edc7be35a47f8afa25e29066d1e3743e30
YazdanPlug <73750616+YazdanPlug@users.noreply.github.com>
2021-01-04 21:08:39 +0330

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,26 +1,41 @@
-package ir.yzplug.CB;
+package iR.yzplug.AS;
 
-//import packages
-import org.cloudburstmc.server.plugin.PluginBase;
-import org.cloudburstmc.server.plugin.PluginManager;
-import ir.yzplug.CB.events.playerChat;
+import org.cloudburstmc.server.event.Listener;
+import org.cloudburstmc.server.event.player.PlayerChatEvent;
+import org.cloudburstmc.server.event.server.ServerInitializationEvent;
+import org.cloudburstmc.server.player.Player;
+import org.cloudburstmc.server.plugin.Plugin;
+import org.cloudburstmc.server.utils.TextFormat;
 
-//public class Main extends PluginBase
-public class Main extends PluginBase {
+@Plugin(id = "AntiSwear", version = "1.1", description = "the players cant swearing in server CloudBurst", authors = "YazdanPlug")
+public class AntiSwear {
 	
-	//@Override and public void onEnable
-	@Override
-	public void onEnable() {
-		this.getLogger().info("Plugin AntiSwear enabled");
-		PluginManager pm = getServer().getPluginManager();
-		pm.registerEvents(new playerChat(), this);
-		super.onEnable();
+	@Listener
+	public void onServerInitialization(ServerInitializationEvent event) {
+	    
 	}
-	//@Override and public void onDisable
-	@Override
-	public void onDisable() {
-		this.getLogger().info("Plugin AntiSwear disabled");
-		super.onDisable();
+	@Listener
+	public void onPlayerChat(PlayerChatEvent event){
+		Player player = event.getPlayer();
+		if (event.getMessage().toLowerCase().contains("fuck")){
+			event.setCancelled(true);
+			player.sendMessage(TextFormat.RED + "You cant swearing in this server. you saying "+TextFormat.YELLOW+"[ fuck ]");
+		}
+		if (event.getMessage().toLowerCase().contains("dick")){
+			event.setCancelled(true);
+			player.sendMessage(TextFormat.RED + "You cant swearing in this server. you saying "+TextFormat.YELLOW+"[ dick ]");
+		}
+		if (event.getMessage().toLowerCase().contains("sex")){
+			event.setCancelled(true);
+			player.sendMessage(TextFormat.RED + "You cant swearing in this server. you saying "+TextFormat.YELLOW+"[ sex ]");
+		}
+		if (event.getMessage().toLowerCase().contains("xxnx")){
+			event.setCancelled(true);
+			player.sendMessage(TextFormat.RED + "You cant swearing in this server. you saying "+TextFormat.YELLOW+"[ xxnx ]");
+		}
+		if (event.getMessage().toLowerCase().contains("poup")){
+			event.setCancelled(true);
+			player.sendMessage(TextFormat.RED + "You cant swearing in this server. you saying "+TextFormat.YELLOW+"[ poup ]");
+		}
 	}
-
 }
